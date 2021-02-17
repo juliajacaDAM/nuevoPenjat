ca3e17dd0afed636a39302fac6fedc877e612e34
Julia Jaca <jjaca@cifpfbmoll.eu>
2021-02-17 17:10:45 +0100

    Crear metodo inicializar partida - creamos array de chat con el primer parametro y lo metemos en el array de char de nuestro obj tablero en palabra secreta -hacemos lo mismo con la palabra pero lo guardamos con espacios en blanco en atrib palabra endivinada - asignamos a intentos y a total intentos el valor del segundo parametro Tarea 3

--- src/com/jaume/penjat/Tauler.java
+++ src/com/jaume/penjat/Tauler.java
@@ -43,4 +43,19 @@ public class Tauler {
     public void setTotalIntents(int totalIntents) {
         this.totalIntents = totalIntents;
     }
+
+    public void inicialitzarPartida(String palabra, int intentos){
+        this.paraulaSecreta = new char[palabra.length()];
+        for(int i =0; i <palabra.length(); i++){
+            this.paraulaSecreta[i] = charAt(i)
+        }
+        this.palabraEndevinada = new String(palabra.length());
+        for(int i = 0 ; i < palabra.length(); i++){
+            if(palabra.charAt(i) == new Character(value = ' ')){
+                this.palabraEndevinada[i] == " ";
+            } 
+        }
+        this.setIntents(intentos);
+        this.setTotalIntents(intentos)
+    }
 }
