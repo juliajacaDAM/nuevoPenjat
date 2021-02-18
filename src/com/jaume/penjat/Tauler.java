534ae2352e6818a3d72f8f5a42d4154c8bfe41bb
Juliajaca <jjaca@cifpfbmoll.eu>
2021-02-18 19:32:11 +0100

    Colocado el constructor debajo de los atributos y corregir errores de código del metodo inicializarPartida -Movido el constructor - Faltaba un } cerrando el metodo - Tenia que llamar a chatat desde palabra - faltaba una punto y coma - error en new character(value = '') Tarea 4

--- src/com/jaume/penjat/Tauler.java
+++ src/com/jaume/penjat/Tauler.java
@@ -5,17 +5,21 @@ public class Tauler {
     private String[] palabraEndevinada;
     private int intents;
     private int totalIntents;
-
+    
+    public Tauler(){
+        paraulaSecreta = new char[0];
+        palabraEndevinada = new String[0];
+        intents = 0;
+        totalIntents = 0;
+    }
+    
     public char[] getParaulaSecreta() {
         return paraulaSecreta;
-
-
     }
 
     public void setParaulaSecreta(char[] paraulaSecreta) {
         this.paraulaSecreta = paraulaSecreta;
     }
-
 
     public String[] getPalabraEndevinada() {
         return palabraEndevinada;
@@ -44,20 +48,16 @@ public class Tauler {
     public void inicialitzarPartida(String palabra, int intentos){
         this.paraulaSecreta = new char[palabra.length()];
         for(int i =0; i <palabra.length(); i++){
-            this.paraulaSecreta[i] = charAt(i)
+            this.paraulaSecreta[i] = palabra.charAt(i);
         }
-        this.palabraEndevinada = new String(palabra.length());
+        this.palabraEndevinada = new String[palabra.length()];
         for(int i = 0 ; i < palabra.length(); i++){
-            if(palabra.charAt(i) == new Character(value = ' ')){
-                this.palabraEndevinada[i] == " ";
+            if(palabra.charAt(i) == new Character(' ')){
+                this.palabraEndevinada[i] = " ";
             } 
         }
+        
         this.setIntents(intentos);
-        this.setTotalIntents(intentos)
-    public Tauler(){
-        paraulaSecreta = new char[0];
-        palabraEndevinada = new String[0];
-        intents = 0;
-        totalIntents = 0;
+        this.setTotalIntents(intentos);
     }
 }
